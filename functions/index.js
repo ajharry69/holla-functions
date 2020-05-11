6cd906f2f95dc8730f1dca366d571fe48a56c62a
Orinda Harrison <ajuongharry35@gmail.com>
2020-05-11 09:01:47 +0300

    add notification sending upon conversation deletion and update

--- functions/index.js
+++ functions/index.js
@@ -4,15 +4,52 @@ const admin = require('firebase-admin');
 admin.initializeApp();
 const db = admin.firestore();
 
-exports.updateConversationsAndUnreadCounts = functions.firestore
+exports.sendNotificationOnConversationUpdate = functions.firestore
+    .document('chats/{ownerId}/conversations/{mateId}')
+    .onUpdate(async (change, context) => {
+        try {
+            const fcmToken = (await db.doc(`users/${context.params.ownerId}`).get()).data().fcmToken;
+            const mateId = context.params.mateId;
+            return sendConversationDeleteOrUpdateNotification(fcmToken, mateId);
+        } catch (e) {
+            console.log(`Conversation update notification error: ${e}`);
+            return null;
+        }
+    })
+
+exports.cleanMessagesOnConversationDeletion = functions.firestore
+    .document('chats/{ownerId}/conversations/{mateId}')
+    .onDelete(async (snapshot, context) => {
+        const ownerId = context.params.ownerId;
+        const mateId = context.params.mateId;
+        // const deletedConversation = snapshot.data();
+        let fcmToken;
+        try {
+            fcmToken = (await db.doc(`users/${ownerId}`).get()).data().fcmToken;
+        } catch (e) {
+            // Ignore...
+        }
+
+        // Do clean up! Delete messages sent to / received by {ownerId} from {mateId}
+        return db.doc(`chats/${ownerId}/messages/${mateId}`).delete()
+            .then(() => {
+                return db.doc(`chats/${ownerId}/unread-count/${mateId}`).delete();
+            }).then(() => {
+                return sendConversationDeleteOrUpdateNotification(fcmToken, mateId, Operation.DELETE);
+            }).catch(err => {
+                console.log('Conversations messages clean up error: ', err);
+                return null;
+            })
+    })
+
+exports.updateConversationsAndSendChatNotification = functions.firestore
     .document('chats/{userId}/messages/{mateId}/messages/{messageId}')
-    .onWrite((change, context) => {
+    .onWrite(async (change, context) => {
         const contextUserId = context.params.userId;
-        const contextMateId = context.params.mateId;
+        // const contextMateId = context.params.mateId;
         const newMessage = change.after.exists ? change.after.data() : null; // Deletion's probably been done!
         const oldMessage = change.before.data();
 
-        console.log('Delete Request: ' + !newMessage); // TODO: delete...
         let promise = null
         if (!newMessage && oldMessage) {
             // TODO: Implement OPTIONAL delete from both parties
@@ -20,10 +57,11 @@ exports.updateConversationsAndUnreadCounts = functions.firestore
             const messageId = oldMessage.id;
             const receiverId = oldMessage['receiverId'];
             const senderId = oldMessage['senderId'];
-            const mateId = contextUserId === senderId ? receiverId : senderId
-            console.log('Context MateID: {' + contextMateId + '} MateID: {' + mateId + '} SenderID: {' + senderId + '}'); // TODO: Delete...
+            const mateId = contextUserId === senderId ? receiverId : senderId;
+            const mate = await getMate(mateId);
+
             // Get {contextUserId}'s conversation with {receiverId}
-            promise = db.doc('chats/' + contextUserId + '/conversations/' + mateId).get()
+            promise = db.doc(`chats/${contextUserId}/conversations/${mateId}`).get()
                 .then(d => {
                     let response = null;
                     if (d.exists) {
@@ -32,7 +70,7 @@ exports.updateConversationsAndUnreadCounts = functions.firestore
                             // Deleted message was also a conversation!
                             // Replace it with the latest message between {contextUserId} & {mateId}
                             // Get latest message by {contextUserId} to the {mateId}
-                            response = db.collection('chats/' + contextUserId + '/messages/' + senderId + '/messages/')
+                            response = db.collection(`chats/${contextUserId}/messages/${mateId}/messages`)
                                 .orderBy('timeSent', 'desc')
                                 .limit(1).get();
                         } // Deleted message was not a conversation!
@@ -40,14 +78,22 @@ exports.updateConversationsAndUnreadCounts = functions.firestore
                     return response;
                 }).then(snapshot => {
                     if (!snapshot) return null;
+                    // noinspection JSUnresolvedVariable
+                    if (snapshot.empty) {
+                        console.log(`No more messages found between ${contextUserId} & ${mateId}! Deleting conversation...`);
+                        // Delete conversation! Probably all the messages in the conversation were deleted
+                        return db.doc(`chats/${contextUserId}/conversations/${mateId}`).delete();
+                    }
                     let promises = []
                     // noinspection JSUnresolvedFunction
                     snapshot.forEach(doc => {
-                        promises.push(db.doc('chats/' + contextUserId + '/conversations/' + mateId).set(doc.data()));
+                        promises.push(db.doc(`chats/${contextUserId}/conversations/${mateId}`)
+                            .set(getMateConversation(doc.data(), mateId, mate)));
                     })
+                    console.log(`Update conversations ${promises.length}x`);
                     return Promise.all(promises);
                 }).catch(err => {
-                    console.log('Error updating conversations after deletion ', err)
+                    console.log('Error updating conversations after deletion ', err);
                 });
         } else {
             // Message was either updated or created
@@ -55,26 +101,34 @@ exports.updateConversationsAndUnreadCounts = functions.firestore
             const senderId = newMessage['senderId'];
 
             if (contextUserId === senderId) {
-                // update message sender's conversations
+                // conversation will be used to populate a in conversations list
+                const senderMate = await getMate(receiverId);
+                const receiverMate = await getMate(senderId);
                 const unreadCountDocRef = db.doc('chats/' + receiverId + '/unread-count/' + senderId);
-                promise = db.doc('chats/' + senderId + '/conversations/' + receiverId).set(newMessage)
+                // update sender's conversation(s) with the receiver
+                promise = db.doc('chats/' + senderId + '/conversations/' + receiverId)
+                    .set(getMateConversation(newMessage, receiverId, senderMate))
                     .then(() => {
                         console.log('Sender {' + senderId + '} conversation added');
+                        // Remove un-necessary properties before saving
+                        delete newMessage.mateId;
+                        delete newMessage.mate;
                         // add message to receiver's messages
                         return db.doc('chats/' + receiverId + '/messages/' + senderId + '/messages/' + newMessage.id)
                             .set(newMessage);
                     }).then(() => {
                         console.log('Receiver {' + receiverId + '} message created');
-                        // update message receiver's conversations
+                        // update message-receiver's conversation(s) with the sender
                         return db.doc('chats/' + receiverId + '/conversations/' + senderId)
-                            .set(newMessage);
+                            .set(getMateConversation(newMessage, senderId, receiverMate));
                     }).then(() => {
+                        console.log('Receiver {' + receiverId + '} conversation created');
                         // Get receiver's current unread count
                         return unreadCountDocRef.get();
                     }).then(doc => {
                         if (oldMessage) {
-                            // Probably an update
-                            if (oldMessage.id === newMessage.id) return null; // Message was update, no need of updating count
+                            // Message was update, no need of updating count
+                            if (oldMessage.id === newMessage.id) return null;
                         }
                         let unreadCount = 1;
                         // noinspection JSUnresolvedVariable
@@ -92,15 +146,46 @@ exports.updateConversationsAndUnreadCounts = functions.firestore
                         }
                         return null;
                     }).catch(err => {
-                            console.log('Error: ' + err);
+                            console.log(`Error: ${err}`);
                         }
                     );
             } else {
+                // DsID: Data Sender ID; CsID: Context Sender ID
                 console.log('DsID not same as CsId! DsID: {' + senderId + '} CsID: {' + contextUserId + '}')
             }
         }
         return promise
     })
+
+async function getMate(mateId) {
+    return (await db.doc(`users/${mateId}`).get()).data();
+}
+
+function getMateConversation(message, mateId, mate) {
+    try {
+        // noinspection JSUnresolvedVariable
+        delete mate.fcmToken;
+    } catch (e) {
+        // Ignore
+    }
+    const conversation = message;
+    conversation.mateId = mateId;
+    conversation.mate = mate;
+    return conversation;
+}
+
+function sendConversationDeleteOrUpdateNotification(receiverTokens, mateId, operation = Operation.UPDATE) {
+    return admin.messaging().sendToDevice(receiverTokens, {
+        data: {
+            mateId: mateId,
+            operation: operation
+        }
+    }).then(response => {
+        return console.log(`Conversation ${operation} message sent: `, response);
+    }).catch(err => {
+        console.log(`Failed to send conversation ${operation} notification: `, err);
+    });
+}
 
 function sendNotification(senderId, receiverTokens, messageData) {
     return admin.messaging().sendToDevice(receiverTokens, {
@@ -113,11 +198,16 @@ function sendNotification(senderId, receiverTokens, messageData) {
             }) // Will be used to initiate a message fetch upon notification reception at the client side
         }
     }, {
-        ttl: 24 * 60 * 60,
+        ttl: 30 * 24 * 60 * 60,
         priority: 'high'
     }).then(response => {
         return console.log('FCM message sent: ', response);
     }).catch(err => {
         console.log('Failed to send notification: ', err);
     });
+}
+
+const Operation = {
+    DELETE: 'DELETE',
+    UPDATE: 'UPDATE'
 }
